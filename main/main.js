767ba0225fc078d7f943373fc800850cfc1acaff
Felipe Suarez <felipesuarez448@gmail.com>
2022-05-11 12:31:23 -0500

    init

---
+++ main/main.js
@@ -0,0 +1,39 @@
+
+
+// -------------------------------------------------------------- ALPHABET SOUP.
+
+new p5();
+
+// -------------------------------------------------------------- VARS.
+
+
+let CANVAS_SIZE = createVector(800, 500);
+
+
+// -------------------------------------------------------------- FUNCTIONS.
+
+
+function setup() {
+  let CNV = createCanvas(CANVAS_SIZE.x, CANVAS_SIZE.y);
+  CNV.parent('canva-game');
+
+  angleMode(DEGREES);
+  frameRate(30);
+  restartGame();
+}
+
+function restartGame() {
+  
+}
+
+function draw() {
+
+}
+
+
+
+
+
+
+
+
