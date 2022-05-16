2439858103ec7de4e277e742906c371a1cd9c81d
Felipe Suarez <felipesuarez448@gmail.com>
2022-05-16 16:07:32 -0500

    new images

--- main/main.js
+++ main/main.js
@@ -23,10 +23,12 @@ Animate the image to fall from the sky.
 const MARGIN = createVector(50, 60);
 
 const DESCRIPTIONS = {
-  'table': 'Where people eat', 
-  'airport': 'Where planes are', 
-  'baseball': 'Sport of hitting a ball with a wooden stick', 
-  'inside': 'It has something outside and...',
+  'bubble': 'A ball formed of air surrounded by liquid that floats in the air.', 
+  'business': 'The activity of buying and selling goods and services.', 
+  'success': 'The achieving of the results wanted or hoped for.',
+  'common': 'The same in a lot of places or for a lot of people.',
+  'yellow': 'A colour like lemon or gold or the sun.',
+  'address': 'The number of the place where a person lives or works.',
 }
 
 let words = Object.keys(DESCRIPTIONS);
@@ -38,15 +40,15 @@ words = words.map(word => word.toUpperCase());
 let images = {
   image_size: createVector(350, 350),
   show_pos: MARGIN.copy(),
-  hide_pos: createVector(-350, MARGIN.y),
+  hide_pos: createVector(-550, MARGIN.y),
   actual_index: 0,
   files: [
-    '../imgs/image1.jpg',
-    '../imgs/image2.jpg',
-    '../imgs/image3.jpg',
-    '../imgs/image4.jpg',
-    '../imgs/image5.jpg',
-    '../imgs/image6.jpg',
+    '../imgs/1.jpg',
+    '../imgs/2.jpg',
+    '../imgs/3.jpg',
+    '../imgs/4.jpg',
+    '../imgs/5.jpg',
+    '../imgs/6.jpg',
   ],
   images: [],
   load: function() { // load all the images in the html.
@@ -78,16 +80,58 @@ let images = {
 
 // ------------------------------------------------------ INPUT PANEL
 
-//let p = createP('Insert letters to complete the word.');
 
-let input = createInput('');
-input.size(50, 50);
-input.input(inputManager);
-const input_id = 'game_input';
-input.id(input_id);
+let input_html;
 
-const input_html = document.getElementById(input_id);
-input_html.focus();
+let input_panel = {
+  p_pos: createVector(MARGIN.x + images.image_size.x + 80 + 70, MARGIN.y + images.image_size.y - 60 - 17), // 40
+  input_pos: createVector(MARGIN.x + images.image_size.x + 80, MARGIN.y + images.image_size.y - 60),
+  button_pos: createVector(MARGIN.x + images.image_size.x + 80 + 70, MARGIN.y + images.image_size.y - 60 + 32),
+
+  p: createP('Try a letter you think the word may contain.'),
+  input: createInput(''),
+  button: createButton('TRY LETTER'),
+
+  setup: function() {
+    this.button.mousePressed(onTryBttn);
+
+
+    this.input.size(50, 50);
+    this.input.input(inputManager);
+    const input_id = 'game_input';
+    this.input.id(input_id);
+
+    input_html = document.getElementById(input_id);
+
+    input_html.focus();
+  },
+  wrong: function(LETTER) {
+    this.p.html("The word doesn't contains " + "'" + LETTER + "' letters. TRY ANOTHER.");
+    this.p.style('color', 'red');
+  },
+  right: function(LETTER) {
+    this.p.html("Good! The word contains " + "'" + LETTER + "' letters. NOW TRY ANOTHER.");
+    this.p.style('color', 'blue');
+  },
+  repeated: function(LETTER) {
+    this.p.html("You already tried letter: " + "'" + LETTER + "'. TRY ANOTHER.");
+    this.p.style('color', 'grey');
+  },
+  hide: function() {
+    this.p.position(-500, -500);
+    this.input.position(-500, -500);
+    this.button.position(-500, -500);
+  },
+  show: function() {
+    this.p.html('Try a letter you think the word may contain.');
+    this.p.style('color', 'black');
+
+    this.p.position(this.p_pos.x, this.p_pos.y);
+    this.input.position(this.input_pos.x, this.input_pos.y);
+    this.button.position(this.button_pos.x, this.button_pos.y);
+  }
+}
+
 
 function inputManager() {
   let word = this.value();
@@ -103,29 +147,6 @@ function inputManager() {
   console.log(this.value());
 }
 
-let try_bttn = createButton('TRY LETTER');
-try_bttn.mousePressed(onTryBttn);
-
-let input_panel = {
-  //p_pos: createVector(MARGIN.x + images.image_size.x + 60 + 70, 150 - 10), // 40
-  input_pos: createVector(MARGIN.x + images.image_size.x + 60, 150),
-  button_pos: createVector(MARGIN.x + images.image_size.x + 60 + 70, 150 + 30),
-  //p: p,
-  input: input,
-  button: try_bttn,
-  hide: function() {
-    //this.p.position(-500, -500);
-    this.input.position(-500, -500);
-    this.button.position(-500, -500);
-  },
-  show: function() {
-    //this.p.position(this.p_pos.x, this.p_pos.y);
-    this.input.position(this.input_pos.x, this.input_pos.y);
-    this.button.position(this.button_pos.x, this.button_pos.y);
-  }
-}
-
-
 
 // ------------------------------------------------------ RESULTS PANEL
 
@@ -133,11 +154,11 @@ let restart_bttn = createButton('RESTART');
 restart_bttn.mousePressed(restart);
 
 let results_panel = {
-  p_result_pos: createVector(MARGIN.x + images.image_size.x + 60, 150),
-  p_subtitle_pos: createVector(MARGIN.x + images.image_size.x + 60, 150 + 40),
-  button_pos: createVector(MARGIN.x + images.image_size.x + 60, 150 + 80),
+  p_result_pos: createVector(MARGIN.x + images.image_size.x + 50, MARGIN.y + images.image_size.y - 110),
+  p_subtitle_pos: createVector(MARGIN.x + images.image_size.x + 50, MARGIN.y + images.image_size.y - 50),
+  button_pos: createVector(MARGIN.x + images.image_size.x + 50, MARGIN.y + images.image_size.y - 60 + 55),
 
-  p_result: createP(''),
+  p_result: createElement('h3'),
   p_subtitle: createP('The word was: '),
   button: restart_bttn,
   hide: function() {
@@ -145,10 +166,21 @@ let results_panel = {
     this.p_subtitle.position(-500, -500);
     this.button.position(-500, -500);
   },
-  show: function(RESUTL, BUTTON_TEXT, WORD) {
-    this.p_result.html(RESUTL);
+  show: function(RESUTL, WORD) {
+    this.p_result.style('font-size', '30px');
+    if (RESUTL == 0) {
+      // GAME OVER
+      this.p_result.style('color', 'red');
+      this.p_result.html('GAME OVER');
+      this.button.html('TRY AGAIN');
+    } else {
+      // YOU WIN
+      this.p_result.style('color', 'blue');
+      this.p_result.html('YOU WIN');
+      this.button.html('NEXT LEVEL');
+    }
+
     this.p_subtitle.html('The word was: ' + WORD);
-    this.button.html(BUTTON_TEXT);
 
     console.log(RESUTL, WORD, 'FILL PARAMETERS');
 
@@ -177,8 +209,9 @@ function letterFild(LETTER, POS) {
 }
 
 let word_filds = {
-  init_pos: createVector(MARGIN.x + images.image_size.x + 15, MARGIN.y + images.image_size.y - 45),
+  init_pos: createVector(MARGIN.x + images.image_size.x + 15, MARGIN.y + (images.image_size.y/2) - 10),
   center: 0,
+  used_letters: '',
   marked_letters: '',
   filds: [],
   setFilds: function(WORD) {
@@ -200,7 +233,7 @@ let word_filds = {
 
     for (const FILD of this.filds) {
       rect(FILD.underline.x, FILD.underline.y, 
-        FILD.underline.width, FILD.underline.height);
+        FILD.underline.width, FILD.underline.height, 20);
 
       //text(FILD.letter, FILD.letter_pos.x, FILD.letter_pos.y);
     }
@@ -210,12 +243,17 @@ let word_filds = {
     for (const FILD of this.filds) {
       if (FILD.letter == LETTER) {
         text(FILD.letter, FILD.letter_pos.x, FILD.letter_pos.y);
+        //this.used_letters += LETTER;
         this.marked_letters += LETTER;
       }
     }
   },
+  addLetter: function(LETTER) {
+    this.used_letters += LETTER;
+  },
   restart: function() {
     this.marked_letters = '';
+    this.used_letters = '';
     this.filds = [];
   },
   autocomplete: function() {
@@ -229,8 +267,10 @@ let word_filds = {
 let lives = {
   counter: 0,
   limit: images.files.length - 1, 
-  //pos: createVector(MARGIN.x + (images.image_size.x/2),MARGIN.y + images.image_size.y),
-  pos: createVector(MARGIN.x + images.image_size.x + 60 + 70, 150 - 10),
+  //pos: createVector(MARGIN.x + (images.image_size.x/2),MARGIN.y + images.image_size.y), // middle wrong
+  //pos: createVector(MARGIN.x + 20, MARGIN.y + images.image_size.y), // left
+  pos: createVector(MARGIN.x + images.image_size.x + 50, MARGIN.y + 30), // title bottom
+
   p: createP('lives: '),
   decrease: function() {
     this.counter++;
@@ -240,6 +280,8 @@ let lives = {
   restart: function() {
     this.p.position(this.pos.x, this.pos.y);
     this.p.html('Lives: ' + this.limit);
+    this.p.style('color', 'blue');
+    this.p.style('font-size', '20px');
     this.counter = 0;
   },
   hide: function() {
@@ -250,13 +292,14 @@ let lives = {
 // ------------------------------------------------------ WORD HINT
 
 let hint = {
-  pos: createVector(0, images.image_size.y + 35), // here goes the word description.
-  p: createP(),
   graphics: createGraphics(windowWidth, windowHeight),
-  set: function(WORD_DESCRIPTION) {
-    this.graphics.textAlign(CENTER, TOP);
+  pos: createVector(0, MARGIN.y + (images.image_size.y/2) + 10), // here goes the word description.
+  draw: function(WORD_DESCRIPTION) {
     this.graphics.clear();
+    this.graphics.textAlign(CENTER, TOP);
+    //this.graphics.textSize(10);
     this.graphics.text('HINT: ' + WORD_DESCRIPTION, word_filds.center, this.pos.y);
+    image(hint.graphics, 0, 0);
   }
   /*
   tengo un canvas solo para esta descripcion, pero puedo acomodarla justo en la mitad de la palabra.
@@ -281,8 +324,8 @@ let alert = {
 
 
 let header = {
-  pos: createVector(MARGIN.x + images.image_size.x + 30, MARGIN.y), // red
-  h: createElement('h3', 'HANGMAN: Complete the word by guessing the letters.'),
+  pos: createVector(MARGIN.x + images.image_size.x + 50, MARGIN.y), // red
+  h: createSpan('COMPLETE THE WORD. SAVE THE MAN.'),
   show: function() {
     this.h.position(this.pos.x, this.pos.y);
   }
@@ -314,6 +357,8 @@ function setup() {
   images.load();
   header.show();
 
+  input_panel.setup();
+
   angleMode(DEGREES);
   frameRate(30);
   restart();
@@ -341,41 +386,36 @@ function restart() {
   word_filds.setFilds(selected_word);
   word_filds.drawFilds();
   
-  hint.set(DESCRIPTIONS[selected_word.toLowerCase()]);
-}
-
-function draw() {
-
+  hint.draw(DESCRIPTIONS[selected_word.toLowerCase()]);
 }
 
 function onTryBttn() {
   if (actual_letter == '') return;
 
   input_html.value = '';
-  
-  if (selected_word.includes(actual_letter)) { // la palabra tiene la letra seleccionada.
 
-    if (word_filds.marked_letters.includes(actual_letter)) { // esta intentado una letra ya marcada antes
-      //titilar las letras anteriormente puestas con color gris.
-      console.log('YOU ALREADY TRIED THIS LETTER');
+  if (word_filds.used_letters.includes(actual_letter)) {
+    //titilar las letras anteriormente puestas con color gris.
+    console.log('YOU ALREADY TRIED THIS LETTER');
+    input_panel.repeated(actual_letter);
 
-    } else { // es la primera vez, asi que esta muy bien.
-      //titilar los espacios vacios con color verde.
-      //rellenar los espacios con la nueva letra.
+  } else if (selected_word.includes(actual_letter)) { // la palabra tiene la letra seleccionada.
+    word_filds.fill(actual_letter); // *** hacer
 
-      word_filds.fill(actual_letter); // *** hacer
-
-      if (word_filds.marked_letters.length == selected_word.length) {
-        //mostrar pantalla de YOU WIN.
-        //deshabilitar el input.
-        input_panel.hide();
-        lives.hide();
-        results_panel.show('YOU WIN', 'NEXT LEVEL', selected_word);
-      }
+    if (word_filds.marked_letters.length == selected_word.length) {
+      //mostrar pantalla de YOU WIN.
+      //deshabilitar el input.
+      input_panel.hide();
+      lives.hide();
+      results_panel.show(1, selected_word);
     }
+    input_panel.right(actual_letter);
+    
   } else { // la palabra no tiene la letra seleccionada.
 
     images.next();
+
+    input_panel.wrong(actual_letter);
 
     lives.decrease();
     console.log(lives.counter, TURNS_LIMIT, 'TURNS INFO');
@@ -388,9 +428,11 @@ function onTryBttn() {
       //deshabilitar el input.
       input_panel.hide();
       lives.hide();
-      results_panel.show('GAME OVER', 'TRY AGAIN', selected_word);
+      results_panel.show(0, selected_word);
     }
   }
+
+  word_filds.addLetter(actual_letter);
 
   actual_letter = '';
 }
@@ -410,10 +452,6 @@ function mouseReleased() {
   //input_panel.show();
   //results_panel.hide();
   input_html.focus();
-}
-
-function draw() {
-  image(hint.graphics, 0, 0);
 }
 
 
